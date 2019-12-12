d2ea19bc3948b3c41bd96797a7b8c31b99c37190
ale-rt <alessandro.pisa@gmail.com>
2019-12-12 15:25:09 +0100

    Remove unnecessary defaultBases

--- src/plone/app/testing/layers.py
+++ src/plone/app/testing/layers.py
@@ -382,8 +382,6 @@ class MockMailHostLayer(Layer):
     """Layer for setting up a MockMailHost to store all sent messages as
     strings into a list at portal.MailHost.messages
     """
-    defaultBases = (PLONE_FIXTURE,)
-
     def testSetUp(self):
         with zope.zopeApp() as app:
             portal = app[PLONE_SITE_ID]
